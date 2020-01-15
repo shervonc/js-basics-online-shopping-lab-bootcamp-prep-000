6a97ba2e70eb6cdb0c45c6ef8e435c6693a5c041
Von C <shervonc@gmail.com>
2020-01-15 14:58:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
- if (cardNumber !== undefined) {
+ if (cardNumber !== null) {
    
    console.log( `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
    for (var i = cart.length; i>= 0; i--) 
@@ -72,7 +72,5 @@ function placeOrder(cardNumber) {
  } else {
    return "Sorry, we don't have a credit card on file for you."
 
-
-  
 }
 }
