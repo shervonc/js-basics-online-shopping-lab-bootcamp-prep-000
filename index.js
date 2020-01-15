40782c9dfb84c92020af0a05c9f4ffa1c4894e16
Von C <shervonc@gmail.com>
2020-01-15 14:58:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
- if (cardNumber !== null) {
+ if (cardNumber) {
    
    console.log( `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
    for (var i = cart.length; i>= 0; i--) 
