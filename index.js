0d7a88221d4b250976c2755d1f7f5cfae4ed3f65
Von C <shervonc@gmail.com>
2020-01-15 14:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ if (cardNumber === undefined) {
    return "Sorry, we don't have a credit card on file for you."
 
   } else {
-   console.log( `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    
+   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
      for (var i = cart.length; i>= 0; i--) 
      cart.pop()
   
