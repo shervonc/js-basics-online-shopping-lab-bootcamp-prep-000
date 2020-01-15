aa1b39a103025c05b46d55a09d13a1b110d4d739
Von C <shervonc@gmail.com>
2020-01-15 14:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
- 
-if (cardNumber === undefined) {
+ if (cardNumber === undefined) {
    return "Sorry, we don't have a credit card on file for you."
 
   } else {
