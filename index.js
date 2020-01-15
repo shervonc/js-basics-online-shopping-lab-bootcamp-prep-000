de6b9150365937aea91a8fa29525f4a85886f6d1
Von C <shervonc@gmail.com>
2020-01-15 14:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ if (cardNumber === undefined) {
 
   } else {
     
-   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   console.log( `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
           for (var i = cart.length; i>= 0; i--) 
           cart.pop()
   
