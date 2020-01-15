de46296ab931fe5ea8bff2f2ebdaa7708627db92
Von C <shervonc@gmail.com>
2020-01-15 14:49:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function placeOrder(cardNumber) {
    return "Sorry, we don't have a credit card on file for you."
 
   } else {
+    
     for (var i = cart.length; i>= 0; i--) 
           cart.pop()
    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
